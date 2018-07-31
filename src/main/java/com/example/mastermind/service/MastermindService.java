e14e38203e8530ced475258f08212f0a65d3dfc0
ABE <abenabdellouahad-nyg@ymagisgroup.com>
2018-07-31 16:11:56 +0100

    optim algo

--- src/main/java/com/example/mastermind/service/MastermindService.java
+++ src/main/java/com/example/mastermind/service/MastermindService.java
@@ -17,101 +17,56 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MastermindService {
 
-	public static final String URL_SERVER = "http://172.16.37.129/api/test";
+	public static final String TEST_URL_SERVER = "http://172.16.37.129/api/test";
+	public static final String START_URL_SERVER = "http://172.16.37.129/api/start";
 	public static final String TOKEN = "token";
-	public static final String RESULT = "result";
 	public static final String NAME = "name";
+	public static final String RESULT = "result";
 	public static final String GOOD = "good";
-	public static final String SUCCESS_VALUE = "5";
+	public static final String SIZE = "size";
+	public static final String NAME_VALUE = "Mastermind7";
+	public static final String TOKEN_VALUE = "tokenmm7";
 
 	public static void execute() throws IOException {
-		String token = "tokenmm7";
-		String goodVal = "";
-		String crt1 = "_";
-		String crt2 = "_";
-		String crt3 = "_";
-		String crt4 = "_";
-		String crt5 = "_";
-		String rsl1 = "";
-		String rsl2 = "";
-		String rsl3 = "";
-		String rsl4 = "";
-		String wordKey = "";
-		System.out.println(new Timestamp(new Date().getTime()));
-		while (!goodVal.equalsIgnoreCase(SUCCESS_VALUE)) {
-			// boucle char 1
-			for (Integer i = 0; i < 10; i++) {
-				crt1 = i.toString();
-				wordKey = crt1 + crt2 + crt3 + crt4 + crt5;
-				Map<String, Object> mapReponse = sendWithMsgBody(URL_SERVER, "POST", buildInfo(token, wordKey));
-				if (null != mapReponse && !mapReponse.isEmpty()) {
-					Integer val = (Integer) mapReponse.get(GOOD);
-					goodVal = val.toString();
-					if (goodVal.equals("1")) {
-						rsl1 = crt1;
-						break;
-					}
-				}
+		// creation start
+		Map<String, Object> mapReponseStart = sendWithMsgBody(START_URL_SERVER, "POST",
+				buildStart(TOKEN_VALUE, NAME_VALUE));
+		if (null != mapReponseStart && !mapReponseStart.isEmpty()) {
+			Integer sizeValue = (Integer) mapReponseStart.get(SIZE);
+			if (null != sizeValue) {
+				sizeValue = 5;
 			}
-			// boucle char 2
-			for (Integer i = 0; i < 10; i++) {
-				crt2 = i.toString();
-				wordKey = rsl1 + crt2 + crt3 + crt4 + crt5;
-				Map<String, Object> mapReponse = sendWithMsgBody(URL_SERVER, "POST", buildInfo(token, wordKey));
-				if (null != mapReponse && !mapReponse.isEmpty()) {
-					Integer val = (Integer) mapReponse.get(GOOD);
-					goodVal = val.toString();
-					if (goodVal.equals("2")) {
-						rsl2 = crt2;
-						break;
-					}
-				}
+			String goodVal = "";
+			String wordKey = "";
+			String[] tab = new String[sizeValue];
+			for (Integer k = 0; k < sizeValue; k++) {
+				tab[k] = "_";
 			}
-			// boucle char 3
-			for (Integer i = 0; i < 10; i++) {
-				crt3 = i.toString();
-				wordKey = rsl1 + rsl2 + crt3 + crt4 + crt5;
-				Map<String, Object> mapReponse = sendWithMsgBody(URL_SERVER, "POST", buildInfo(token, wordKey));
-				if (null != mapReponse && !mapReponse.isEmpty()) {
-					Integer val = (Integer) mapReponse.get(GOOD);
-					goodVal = val.toString();
-					if (goodVal.equals("3")) {
-						rsl3 = crt3;
-						break;
+			System.out.println(new Timestamp(new Date().getTime()));
+			while (!goodVal.equalsIgnoreCase(sizeValue.toString())) {
+				for (Integer j = 0; j < sizeValue; j++) {
+					for (Integer i = 0; i < 10; i++) {
+						tab[j] = i.toString();
+						wordKey = castTab(tab);
+						// if("12345".equalsIgnoreCase(wordKey)) {
+						// System.out.println(wordKey);
+						// }
+						Map<String, Object> mapReponse = sendWithMsgBody(TEST_URL_SERVER, "POST",
+								buildInfo(TOKEN_VALUE, wordKey));
+						if (null != mapReponse && !mapReponse.isEmpty()) {
+							Integer val = (Integer) mapReponse.get(GOOD);
+							goodVal = val.toString();
+							if (goodVal.equals(new Integer(j + 1).toString())) {
+								break;
+							}
+						}
 					}
-				}
-			}
-			// boucle char 4
-			for (Integer i = 0; i < 10; i++) {
-				crt4 = i.toString();
-				wordKey = rsl1 + rsl2 + rsl3 + crt4 + crt5;
-				Map<String, Object> mapReponse = sendWithMsgBody(URL_SERVER, "POST", buildInfo(token, wordKey));
-				if (null != mapReponse && !mapReponse.isEmpty()) {
-					Integer val = (Integer) mapReponse.get(GOOD);
-					goodVal = val.toString();
-					if (goodVal.equals("4")) {
-						rsl4 = crt4;
-						break;
-					}
-				}
-			}
-			// boucle char 5
-			for (Integer i = 0; i < 10; i++) {
-				crt5 = i.toString();
-				wordKey = rsl1 + rsl2 + rsl3 + rsl4 + crt5;
-				Map<String, Object> mapReponse = sendWithMsgBody(URL_SERVER, "POST", buildInfo(token, wordKey));
-				if (null != mapReponse && !mapReponse.isEmpty()) {
-					Integer val = (Integer) mapReponse.get(GOOD);
-					goodVal = val.toString();
-					if (goodVal.equals(SUCCESS_VALUE)) {
-						break;
-					}
-				}
-			}
 
+				}
+			}
+			System.out.println(wordKey);
 		}
 		System.out.println(new Timestamp(new Date().getTime()));
-		System.out.println(wordKey);
 
 	}
 
@@ -159,6 +114,28 @@ public class MastermindService {
 		build.append(RESULT).append("\":\"").append(result);
 		build.append("\"}");
 		return build.toString();
+	}
+
+	private static String buildStart(String token, String name) {
+		StringBuilder build = new StringBuilder();
+		build.append("{\"");
+		build.append(TOKEN);
+		build.append("\": \"");
+		build.append(token).append("\",\"");
+		build.append(SIZE);
+		build.append("\": \"");
+		build.append("5").append("\",\"");
+		build.append(NAME).append("\":\"").append(name);
+		build.append("\"}");
+		return build.toString();
+	}
+
+	private static String castTab(String[] tab) {
+		String result = "";
+		for (String str : tab) {
+			result = result + str;
+		}
+		return result;
 	}
 
 }
