69042c2dc58209e1ba11e5ede948c81885146b86
ABE <abenabdellouahad-nyg@ymagisgroup.com>
2018-08-02 11:12:37 +0100

    code cov

--- src/test/java/com/example/mastermind/main/MainTest.java
+++ src/test/java/com/example/mastermind/main/MainTest.java
@@ -1,16 +1,24 @@
 package com.example.mastermind.main;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 
 import org.junit.Test;
 
+import com.example.mastermind.service.MastermindService;
+import com.example.mastermind.utils.Utilitaires;
+
 public class MainTest {
 	
 	@Test
-	public void contextLoads() throws IOException {
-		assertNull(null);
+	public void mainTest() throws IOException {
+		Integer sizeValue = Utilitaires.getSize();
+		String result = MastermindService.execute(sizeValue);
+		assertNotEquals(result, "");
 	}
+	
+	
 
 }
